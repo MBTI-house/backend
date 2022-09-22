f7539ac0db94d16b1e17a29aa0b0befdcc9625c2
bluewow <dogseen@gmail.com>
2022-09-23 00:01:44 +0900

    first commit

---
+++ src/main/java/com/house/mbit/MbitApplication.java
@@ -0,0 +1,13 @@
+package com.house.mbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/house/mbit/MbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.house.mbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
