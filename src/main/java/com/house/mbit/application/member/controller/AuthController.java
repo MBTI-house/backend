1f1e5f34ec91b7787da414edc1a971b310cd4420
bluewow <dogseen@gmail.com>
2022-10-10 01:39:41 +0900

    refactor: client 에서 accessToken 을 이용한 정보가져오기 처리도 병행 -> 서버에서는 해당 정보를 가져와 처리한다

--- src/main/java/com/house/mbit/application/member/controller/AuthController.java
+++ src/main/java/com/house/mbit/application/member/controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.house.mbit.application.member.controller;
 
+import com.house.mbit.application.member.dto.UserInfoDto;
 import com.house.mbit.core.common.ApiResponse;
 import com.house.mbit.core.jwt.JwtTokenProvider;
 import com.house.mbit.core.util.CookieUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,11 +21,9 @@ public class AuthController {
 
     private final JwtTokenProvider jwtTokenProvider;
 
+
     @PostMapping("/auth/login")
-    public ApiResponse login(String code, HttpServletResponse response) {
-        //https://velog.io/@bgg01578/%EC%B9%B4%EC%B9%B4%EC%98%A4-%EB%A1%9C%EA%B7%B8%EC%9D%B8
-        //https://mangchhe.github.io/springboot/2021/07/18/SpringKakaoLogin/
-        //TODO 카카오 인가코드 처리
+    public ApiResponse login(@RequestBody UserInfoDto userInfoDto, HttpServletResponse response) {
         //TODO 최초 로그인시 회원가입, 또는 유저체크(로그인을 위한)
         response.addCookie(CookieUtil.generator(ACCESS_TOKEN, null));
         response.addCookie(CookieUtil.generator(REFRESH_TOKEN, null));
---
+++ src/main/java/com/house/mbit/application/member/dto/UserInfoDto.java
@@ -0,0 +1,11 @@
+package com.house.mbit.application.member.dto;
+
+import lombok.Data;
+
+@Data
+public class UserInfoDto {
+    public String userId;
+    public String email;
+    public String nickname;
+
+}
